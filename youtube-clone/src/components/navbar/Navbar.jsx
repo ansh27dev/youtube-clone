e427327cab717c5236da5a57e1dfdc7d9e352160
ansh27dev <ansh255gupta@gmail.com>
2024-05-12 19:34:24 +0530

    setting up the project

---
+++ youtube-clone/src/components/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Navbar.css'
+
+const navbar = () => {
+  return (
+    <div>
+      <nav className='flex-div'>
+      </nav>
+    </div>
+  )
+}
+
+export default navbar
---
+++ youtube-clone/src/pages/video/video.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './video.css'
+
+const video = () => {
+  return (
+    <div>video</div>
+  )
+}
+
+export default video
